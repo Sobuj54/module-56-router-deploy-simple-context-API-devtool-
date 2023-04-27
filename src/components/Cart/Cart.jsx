44c39016b1811367c00d76c8908e1e194ad59a70
soubj <sobujahmed124@gmail.com>
2023-04-27 12:25:53 +0600

    conditional rendering using if else

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+  // conditional rendering
+  let message;
+  if (cart.length === 0) {
+    message = <p>Please add products.</p>;
+  } else {
+    message = (
+      <div>
+        <h4>Thanks for buying our products.</h4>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h3>order summary : {cart.length}</h3>
+      {/* this message is the result of conditional rendering */}
+      {message}
       {cart.map((tshirt) => (
         <p>
           {tshirt.name}
