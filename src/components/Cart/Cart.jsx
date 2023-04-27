eebbb37f1b6eb5e3ed16c324540111cb65468b98
soubj <sobujahmed124@gmail.com>
2023-04-27 12:32:17 +0600

    conditional rendering using && operator

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -16,16 +16,21 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
   return (
     <div>
       <h3>order summary : {cart.length}</h3>
+
       {/* conditional rendering using ternary operator */}
       {cart.length > 2 ? <p>valoi kinsen</p> : <p>fokira</p>}
+
       {/* this message is the result of conditional rendering */}
       {message}
+
       {cart.map((tshirt) => (
         <p>
           {tshirt.name}
           <button onClick={() => handleRemoveFromCart(tshirt._id)}>X</button>
         </p>
       ))}
+
+      {cart.length === 2 && <h5>double buy!!</h5>}
     </div>
   );
 };
